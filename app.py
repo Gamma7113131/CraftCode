6d7984ee3a9076143e35f895e2658df4bbc3897c
Gamma7113131 <gametron2012@yahoo.com>
2024-10-16 16:52:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from flask import Flask, render_template, send_from_directory
 import os
 
 # Create the Flask app
-app = Flask(__name__,
-            static_folder="scratch-gui/build/static",  # Point to the static directory in the build
-            template_folder="scratch-gui/build")  # Use build for templates as well
+app = Flask(__name__)
 
 # Suppress Flask's default logging
 log = logging.getLogger('werkzeug')  # This is the default logger for Flask
@@ -17,11 +15,14 @@ def index():
 
 @app.route('/static/<path:filename>')
 def static_files(filename):
+    # Serve static files from the 'static' directory within the Flask app's context
     return send_from_directory(os.path.join(app.static_folder, 'static'), filename)
 
 @app.route('/<path:filename>')
 def serve_build_files(filename):
-    return send_from_directory(app.template_folder, filename)
+    # Serve files from the main application folder
+    return send_from_directory(app.root_path, filename)
 
 if __name__ == '__main__':
+    # Run the Flask application without extra logging
     app.run(host='0.0.0.0', port=5000, debug=False)  # Set debug=False to disable debug output
